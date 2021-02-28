fda176ee369c1412fb8dfb7446c3c131d0d43af9
Binabh <binabhdevkota@gmail.com>
2021-02-28 12:08:38 +0545

    can add nested routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 import Login from '../views/Login';
 import Register from '../views/Register';
 import Dashboard from '../views/Dashboard';
+import Topic from '../views/Topic';
+import SubTopic from '../views/SubTopic';
 
 const indexRoutes = [
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
+  {
+    path: '/topics',
+    name: 'Topic',
+    component: [
+      { path: '/topics/subtopic', name: 'Sub Topic', component: SubTopic },
+      { path: null, name: 'Topic', component: Topic },
+    ],
+  },
   {
     path: '/',
     name: 'Dashboard',
